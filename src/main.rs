1ccb59be77b9e5c0927bfdb9564d9927a34b0692
m0dulo <cuckooegret@gmail.com>
2019-01-16 20:19:15 +0800

    add perlin noise

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 #[macro_use]
 extern crate log;
 
+#[macro_use]
+extern crate lazy_static;
+
 mod renderer;
 mod scenes;
 mod tracer;
 
 use self::renderer::utils::render_high_quality as render;
-use self::scenes::legacy_scene::legacy_scene_texture as scene;
+use self::scenes::simple_scene::simple_scene_perlin_noise as scene;
 use std::env;
 
 fn main() -> Result<(), std::io::Error> {
-    env::set_var("RUST_LOG", "raytracer=info");
-    pretty_env_logger::init_custom_env("RUST_LOG");
+    pretty_env_logger::init();
     info!("generating scene...");
     let (world, camera) = scene();
-    render(world, camera, "legacy_scene_texture.png")?;
+    render(world, camera, "simple_scene_perlin_noise.png")?;
     Ok(())
 }
