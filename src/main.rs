4cc98050bfb1f64e42c706b65407af64ea758fae
m0dulo <cuckooegret@gmail.com>
2019-01-18 16:00:36 +0800

    add rect & cornell box

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,14 @@ mod renderer;
 mod scenes;
 mod tracer;
 
-use self::renderer::utils::render_preview as render;
-use self::scenes::simple_scene::simple_scene_perlin_noise_with_light as scene;
+use self::renderer::utils::render_high_quality as render;
+use self::scenes::cornell_box::cornell_box as scene;
 use std::env;
 
 fn main() -> Result<(), std::io::Error> {
     pretty_env_logger::init();
     info!("generating scene...");
     let (world, camera) = scene();
-     render(
-        world,
-        camera,
-        "simple_scene_perlin_noise_with_light.png",
-        false,
-    )?;
+    render(world, camera, "cornell_box.png", false)?;
     Ok(())
 }
